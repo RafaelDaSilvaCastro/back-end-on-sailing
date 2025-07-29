ca55b7e21f86dddc66d534fec7212e0724454f0e
Rafael Castro <rafaeldasilvacastroti@gmail.com>
2025-07-29 19:00:36 -0300

    Criando endpoint inicial da API

---
+++ Routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  res.status(200).json({ message: "Estamos navegando!🛥️" } );
+} );
+
+export default router;
--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import authRoutes from './Routes/authRoutes'
+import indexRoutes from './Routes/index';
 
 const app = express();
 app.use(express.json());
 
 app.use('/auth', authRoutes);
+app.use('/', indexRoutes);
 
 app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000');
