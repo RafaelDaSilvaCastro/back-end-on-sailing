a04185e7ec27646e99917333ecf1825dbc8b9b20
Rafael Castro <rafaeldasilvacastroti@gmail.com>
2025-08-01 20:41:29 -0300

    Aplicando rate limit

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 import authRoutes from './Routes/authRoutes'
 import indexRoutes from './Routes/index';
 
 const app = express();
 app.use(express.json());
+
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes     
+  max: 100,
+  standardHeaders: true, 
+  legacyHeaders: false, 
+  message: { error: "Muitas requisições, tente novamente mais tarde." }
+});
+app.use(globalLimiter);
 
 app.use('/auth', authRoutes);
 app.use('/', indexRoutes);
